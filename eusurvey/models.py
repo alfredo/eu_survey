81b2b2262e1ecc20ef9bad634649d45829695c7d
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-01 23:47:45 +0000

    Update models definitions.

--- eusurvey/models.py
+++ eusurvey/models.py
@@ -6,13 +6,24 @@ PreSubmission = namedtuple('PreSubmission', ['cookies', 'payload'])
 
 SuccessResponse = namedtuple('SuccessResponse', ['uid', 'url'])
 
-Field = namedtuple('Field', [
-    'name',
-    'label',
+Label = namedtuple('Label', [
+    'text',
+    'pk',
 ])
 
+Field = namedtuple('Field', [
+    'label',
+    'widget',
+])
+
+FormSet = namedtuple('FormSet', [
+    'question',
+    'field_list',
+    'is_mandatory',
+])
 
 Form = namedtuple('Form', [
     'title',
     'sections',
+    'formset_list',
 ])
