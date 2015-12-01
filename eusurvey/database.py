d77c3175d1f6373a66e3cf24219a75f240b59f62
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-01 22:30:50 +0000

    Rename file storage functions.

--- eusurvey/database.py
+++ eusurvey/database.py
@@ -1,19 +1,23 @@
-import os
-
 import codecs
+import logging
+import os
 
 from eusurvey import settings
 
+logger = logging.getLogger(__name__)
 
-def save_output(stream, name='output.html'):
+
+def save_stream(stream, name='output.html'):
     file_path = os.path.join(settings.DB_ROOT, name)
-    assert False, file_path
+    logger.debug('Saving stream: `%s`', file_path)
     with codecs.open(file_path, 'w', 'utf-8') as f:
         f.write(stream.decode('utf-8'))
     return f
 
 
-def read_output(name="output.html"):
+def read_file(name="output.html"):
     file_path = os.path.join(settings.DB_ROOT, name)
+    logger.debug('Reading stream: `%s`', file_path)
     with codecs.open(file_path, 'r', 'utf-8') as f:
         return f.read()
+    raise ValueError('Something went wrong with: `%s`' % file_path)
