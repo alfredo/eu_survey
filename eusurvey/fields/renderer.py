598cec18539f438f68405ce02503e78fe04249f0
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-04 01:18:17 +0000

    Join the renderer in a single output.

--- eusurvey/fields/renderer.py
+++ eusurvey/fields/renderer.py
@@ -4,15 +4,18 @@ logger = logging.getLogger(__name__)
 
 
 def render(formset_list):
+    output = []
     for i, formset in enumerate(formset_list, start=1):
-        logger.info('%s. `%s`', i, formset.question)
+        output.append('%s. `%s`' % (i, formset.question))
         if formset.field_list:
             for field in formset.field_list:
-                logger.info('    - %s', field['label'])
-        if formset.field:
-            msg = '^^ (Supplementary)' if formset.is_supplementary else ''
-            logger.warning('%s', msg)
+                output.append('    - %s' % field['label'])
         if formset.option_list:
             for option in formset.option_list:
-                logger.info('    - %s', option['text'])
-    return True
+                output.append('    - %s' % option['text'])
+        msg = '      ^^ (Supplementary)' if formset.is_supplementary else ''
+        if msg:
+            output.append(msg)
+    result = '\n'.join(output)
+    logger.info(result)
+    return result
