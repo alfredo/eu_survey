ac2843391b38cb39250ea535c77f9b8f4d5c4b65
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-04 00:20:30 +0000

    Update extractor calculation.

--- eusurvey/fields/extractors/checkbox.py
+++ eusurvey/fields/extractors/checkbox.py
@@ -8,8 +8,13 @@ from eusurvey.fields.common import (
 def get_input(element):
     element = g(element.xpath('.//td/input'))
     _a = lambda x: element.attrib.get(x)
-    attrs = ['id', 'name', 'data-id', 'data-dependencies', 'value']
+    attrs = ['id', 'name', 'data-id', 'value']
     field = dict([(k, _a(k)) for k in attrs])
+    if 'data-dependencies' in element.attrib:
+        dependencies = _a('data-dependencies').split(';')
+    else:
+        dependencies = None
+    field['data-dependencies'] = dependencies
     field['type'] = 'checkbox'
     return field
 
--- eusurvey/fields/extractors/select.py
+++ eusurvey/fields/extractors/select.py
@@ -5,9 +5,14 @@ def get_option_list(section):
     option_list = []
     for option in section.xpath('.//option'):
         _a = lambda x: option.attrib.get(x)
-        attrs = ['id', 'name', 'data-id', 'data-dependencies', 'value']
+        attrs = ['id', 'name', 'data-id', 'value']
         field = dict([(k, _a(k)) for k in attrs])
         field['text'] = option.text.strip() if option.text else None
+        if 'data-dependencies' in option.attrib:
+            dependencies = _a('data-dependencies').split(';')
+        else:
+            dependencies = None
+        field['data-dependencies'] = dependencies
         field['type'] = 'option'
         option_list.append(field)
     return option_list
