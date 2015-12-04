16e8717b5dbafd807f2d22ea3d576f71c289e40e
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-04 22:30:42 +0000

    Add content extractor mechanics.

---
+++ eusurvey/content.py
@@ -0,0 +1,15 @@
+from eusurvey.fields.common import to_str, get
+
+
+PATTERN_LIST = [
+    u'.//div[contains(@class, "sectiontitle")]',
+    u'.//div[@class="text"]',
+]
+
+
+def extractor(section):
+    for pattern in PATTERN_LIST:
+        result = section.xpath(pattern)
+        if result:
+            return get(section.xpath(pattern)).text_content()
+    return None
