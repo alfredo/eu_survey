1e9df5b41b25d2ef34d7cdeca99be1136f76fb47
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-06 17:24:59 +0000

    Remoove filename as optional params.

--- eusurvey/database.py
+++ eusurvey/database.py
@@ -7,7 +7,7 @@ from eusurvey import settings
 logger = logging.getLogger(__name__)
 
 
-def save_stream(stream, name='output.html'):
+def save_stream(stream, name):
     file_path = os.path.join(settings.DB_ROOT, name)
     logger.debug('Saving stream: `%s`', file_path)
     with codecs.open(file_path, 'w', 'utf-8') as f:
@@ -15,7 +15,7 @@ def save_stream(stream, name='output.html'):
     return file_path
 
 
-def read_file(name="output.html"):
+def read_file(name):
     file_path = os.path.join(settings.DB_ROOT, name)
     logger.debug('Reading stream: `%s`', file_path)
     with codecs.open(file_path, 'r', 'utf-8') as f:
