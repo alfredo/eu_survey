967b53286dc9bbd2b48f35f452364a0e93829ca2
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-06 09:56:32 +0000

    Update common functions docstrings.

--- eusurvey/limesurvey/common.py
+++ eusurvey/limesurvey/common.py
@@ -5,14 +5,22 @@ def get_missing(row, total):
 
 
 def get_name(name):
-    # Shortens the name of the answer by removing the prefix
+    """Shortens the name of the answer by removing the prefix"""
     return name.replace('answer', 'a')
 
 
 def get_value(value):
-    # Removes the first 4 chars that can be pick from the answer name:
-    return value[4:]
+    """Returns a unique number for the value.
+
+    All values have consecutive numbers e.g.
+
+    `6251626` can be represented by `1626`.
+
+    By using this notation it allows up to `9999` fields to be used
+    in the form."""
+    return value[3:]
 
 
 def get_mandatory(formset):
+    """Generates the mandatory field representation for LimeSurvey."""
     return 'Y' if formset.is_mandatory else ''
