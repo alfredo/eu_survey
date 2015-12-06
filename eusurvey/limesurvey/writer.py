4c5aa0ced96fceb768615c5cb2180a3d39f9a5f1
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-06 17:19:02 +0000

    Update survey export saving mechanics.

--- eusurvey/limesurvey/writer.py
+++ eusurvey/limesurvey/writer.py
@@ -9,10 +9,10 @@ logger = logging.getLogger(__name__)
 
 
 def save(row_list):
-    path = os.path.join(settings.DB_ROOT, 'survey-generated.txt')
+    path = os.path.join(settings.DB_ROOT, 'limesurvey-form.txt')
     with open(path, 'w') as stream:
         writer = csv.UnicodeWriter(stream)
         for row in row_list:
             writer.writerow(row)
-    logger.info('LimeSurvey export saved: `%s`' % path)
+    logger.info('LimeSurvey CSV importable form saved in: `%s`' % path)
     return path
