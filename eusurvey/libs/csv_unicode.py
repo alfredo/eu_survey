dcbed658fdbdf03af731a6a96137e244d8b7665b
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-06 19:45:54 +0000

    Change CSV reader schema to be excel.
    Exports are comma separated.

--- eusurvey/libs/csv_unicode.py
+++ eusurvey/libs/csv_unicode.py
@@ -23,7 +23,7 @@ class UnicodeReader:
     """A CSV reader which will iterate over lines in the CSV file "f",
     which is encoded in the given encoding."""
 
-    def __init__(self, f, dialect=TabExcelDialect, encoding="utf-8", **kwds):
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
         f = UTF8Recoder(f, encoding)
         self.reader = csv.reader(f, dialect=dialect, **kwds)
 
