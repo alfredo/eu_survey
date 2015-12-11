de3a6ad982ae2827e659472e44e1a645e38b6294
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-11 21:03:41 +0000

    Handle exceptions during importing.

--- eusurvey/engine.py
+++ eusurvey/engine.py
@@ -1,12 +1,19 @@
+import logging
+
 from eusurvey.fields import importer
 from eusurvey.poster import submission
+
+logger = logging.getLogger(__name__)
 
 
 URL = 'https://ec.europa.eu/eusurvey/runner/Platforms/'
 
 
 def import_survey(url):
-    importer.process(url)
+    try:
+        importer.process(url)
+    except ValueError, e:
+        logger.error(e)
     return True
 
 
