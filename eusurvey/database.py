7a4c78b9a065d1db2c87d4b033372cfbbafdb785
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-11 21:07:58 +0000

    Add mechanics to init the local DB.

--- eusurvey/database.py
+++ eusurvey/database.py
@@ -21,3 +21,20 @@ def read_file(name):
     with codecs.open(file_path, 'r', 'utf-8') as f:
         return f.read()
     raise ValueError('Something went wrong with: `%s`' % file_path)
+
+
+def init_db(survey):
+    """Prepares the storage for the given survey."""
+    survey_path = os.path.join(settings.DB_ROOT, survey['name'])
+    if os.path.exists(survey_path):
+        logger.error(
+            'Survey has already been imported: See: `%s`', survey_path)
+        return False
+    path_list = (
+        survey_path,
+        os.path.join(survey_path, 'submissions'),
+    )
+    for path in path_list:
+        logger.debug('Directory created: `%s`', path)
+        os.mkdir(path)
+    return True
