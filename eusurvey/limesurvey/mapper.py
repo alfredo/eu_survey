4aa2a7a97a01af64fdb5a2178324a532e398c5eb
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-12 16:42:29 +0000

    Generate mapper file from the exported limesurvey.

--- eusurvey/limesurvey/mapper.py
+++ eusurvey/limesurvey/mapper.py
@@ -1,2 +1,21 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+RELEVANT_ROWS = [0, 1, 2, 4]
+
+
 def create_mapper(survey_list):
-    return survey_list
+    mapper_list = [
+        ['class', 'type/scale', 'name', 'translation', 'text']
+    ]
+    for survey in survey_list:
+        row = [survey[n] for n in RELEVANT_ROWS]
+        # Remove text for page rows:
+        if row[0] in ['G']:
+            row[-1] = ''
+        # Placeholder for the value translation:
+        row.insert(3, '')
+        mapper_list.append(row)
+    logger.info('Mapped rows: %s', len(mapper_list))
+    return mapper_list
