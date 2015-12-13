60559537f7193a4d39a61d01528cd1f7173503c7
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-13 00:02:03 +0000

    Bugfix: Add support for multiple radio buttions.

--- eusurvey/limesurvey/postprocessor.py
+++ eusurvey/limesurvey/postprocessor.py
@@ -60,16 +60,19 @@ def get_field_condition(field_id, row_list=None):
         # Matrix fields. They are compressed into a row per subquestion/answer
         # For this reason they have multiple ids:
         q_subtype = question[1].strip()
-        if isinstance(metadata['field_id'], list) and q_subtype in ['F']:
-            logger.debug(metadata['field_id'])
+        matrix_conditions = [
+            isinstance(metadata['field_id'], list),
+            q_subtype in ['F'],
+            field_id in metadata['field_id'],
+        ]
+        if all(matrix_conditions):
             question_name = common.get_row_value(question, 'name')
             row_value = common.get_row_value(row, 'name')
             condition_list = []
             for subquestion in subquestion_list:
                 condition_list.append(
                     "%s_%s == '%s'" % (question_name, subquestion,  row_value))
-            conditions = ' OR '.join(set(condition_list))
-            logger.warning(conditions)
+            conditions = ' OR '.join(sorted(condition_list))
             return conditions
     logger.error('Missing triggers for: `%s`', field_id)
     return None
@@ -84,18 +87,22 @@ CONDITION_REPLACEMENTS = {
 
 
 def add_dependencies(row_list):
+    """Adds any dependency between the given rows using the metadata."""
     get_condition = partial(get_field_condition, row_list=row_list)
     for i, row in enumerate(row_list):
         if not has_metadata(row):
-            # Row has no metadata, ignore:
+            # Row has no dependencies. Ignore.
             continue
         metadata = row[-1]
+        # `triggers` are the fields that activate this field:
         if 'triggers' in metadata and metadata['triggers']:
+            logger.info('%s - %s', i, metadata['triggers'])
             conditions = set([get_condition(t) for t in metadata['triggers']])
             conditions = filter(None, conditions)
             if conditions:
                 conditions = sorted(conditions)
                 conditions_str = ' OR '.join(conditions)
+                logger.debug(conditions_str)
                 if conditions_str in CONDITION_REPLACEMENTS:
                     new_str = CONDITION_REPLACEMENTS[conditions_str]
                     logger.info('Replacing `%s` with `%s`', conditions_str, new_str)
