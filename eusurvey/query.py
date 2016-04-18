b4b25b0cf918ac9876abe16d540bebb8fab40717
Alfredo Aguirre <alfredo@madewithbyt.es>
2016-04-18 16:38:52 +0100

    Extract all the languages available for a given survey.
    The languages are determined by the drop down menu
    available in the initial survey HTML.
    
    Then each survey is downloaded using the `surveylanguage`
    parameter and added as a `translations` key in the survey_dict.

--- eusurvey/query.py
+++ eusurvey/query.py
@@ -30,6 +30,14 @@ def get_form_title(tree):
     return title_text
 
 
+def get_language_list(tree, ignore_list=None):
+    elements = tree.xpath('.//select[@id="langSelectorRunner"]/option')
+    language_list = [e.attrib.get('value') for e in elements]
+    if ignore_list:
+        language_list = filter(lambda l: l not in ignore_list, language_list)
+    return language_list
+
+
 def get_survey_name(url):
     """Determines the survey name."""
     return slugify(url, only_ascii=True)
@@ -38,13 +46,17 @@ def get_survey_name(url):
 def get_survey_dict(url):
     """Extracts the survey information."""
     form_tree = get_form_tree(url)
-    title = get_form_title(form_tree.tree)
     name = get_survey_name(url)
+    language_list = get_language_list(
+        form_tree.tree, ignore_list=[form_tree.language])
+    _lang_form = lambda l: get_form_tree(url, params={'surveylanguage': l})
+    translations = [_lang_form(l) for l in language_list]
     return {
+        'id': None,
         'url': url,
         'name': name,
-        'title': title,
+        'title': get_form_title(form_tree.tree),
         'form_tree': form_tree,
-        'id': None, # TODO: get form ID
+        'translations': translations,
         'survey_path': os.path.join(settings.DB_ROOT, name),
     }
