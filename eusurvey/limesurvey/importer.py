2d69c8a035c70d89eb7fe9f34302b41d1deaa4b9
Alfredo Aguirre <alfredo@madewithbyt.es>
2016-04-19 14:01:44 +0100

    Add language update during limesurvey transformations.
    The language in the translated fields is updated post
    limesurvey convertion, this was made because updating the
    language on each of the filters could be a more complicated
    task.
    
    Since the mark up for the translations is the same updating
    the language after the limesurvey content has been extracted
    produces the same output.

--- eusurvey/limesurvey/importer.py
+++ eusurvey/limesurvey/importer.py
@@ -28,11 +28,12 @@ def get_settings_rows(total):
     return settings_rows
 
 
-def get_local_settings_rows(total):
+def get_local_settings_rows(total, language):
     settings_rows = []
     for row in constants.EN_SETTINGS:
         missing = common.get_missing(row, total)
         full_row = list(row) + missing
+        full_row[6] = language
         settings_rows.append(full_row)
     return settings_rows
 
@@ -84,7 +85,17 @@ def prepare_formset_list(formset_list, total):
     return prepared_formset_list
 
 
-def prepare_survey_list(survey_list, total):
+def update_language(survey_list, language):
+    """Updates the language colum in the given survey_list."""
+    updated_list = []
+    # Column 6 contains the language definition:
+    for item in survey_list:
+        item[6] = language
+        updated_list.append(item)
+    return updated_list
+
+
+def prepare_survey_list(survey_list, total, language='en'):
     prepared_survey_list = []
     for i, (page, formset_list) in enumerate(survey_list):
         prepared_survey_list.append(get_page_row(page, total, formset_list))
@@ -94,16 +105,25 @@ def prepare_survey_list(survey_list, total):
         partial_list = prepare_formset_list(formset_list, total)
         if partial_list:
             prepared_survey_list += partial_list
+    # Update language for the survey_list
+    prepared_survey_list = update_language(prepared_survey_list, language)
     return prepared_survey_list
 
 
-def convert_survey_list(survey_list):
-    """Transform the survey elements into a list of LimeSurvey rows."""
+def convert_survey_list(survey_list, language):
+    """Transform the survey elements into a list of LimeSurvey rows.
+
+    Returns:
+    - `full_list`: Tuples ready compatible with limesurvey import file.
+       Contains all the required fields.
+    - `questions`: Questions contained on the survey list.
+    """
+    # TODO: Determine dynamically name and details of the imported survey.
     total = len(constants.COLUMNS)
     row_list = [constants.COLUMNS]
     row_list += get_settings_rows(total)
-    row_list += get_local_settings_rows(total)
-    survey_list = prepare_survey_list(survey_list, total)
+    row_list += get_local_settings_rows(total, language)
+    survey_list = prepare_survey_list(survey_list, total, language)
     row_list += survey_list
     updated_row_list = postprocessor.update_row_list(row_list)
     return {
